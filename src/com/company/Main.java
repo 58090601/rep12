7db1af3d4959cf430f28db8a70c9cc75a3e39fe9
09060115 <aspirenkov25@gmail.com>
2022-04-23 14:17:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Введите ip-адрес:");
+        Scanner sc=new Scanner(System.in);
+        String ip= sc.nextLine();
+        String input=ip;
+        boolean result=input.matches("(((2[0-5]{2})|(2[0-4]\\d)|1\\d{2}|\\d{2}|\\d)\\.){3}((2[0-5]{2})|(2[0-4]\\d|1\\d{2}|\\d{2}|\\d))");
+        if (result){
+            System.out.println("Правильный IP");
+            try (BufferedWriter f = new BufferedWriter(new FileWriter("C://12//input.txt"))) {
+                f.write(input);
+                System.out.println("Файл создан");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        else {
+            System.out.println("ОШИБКА");
+        }
+    }
+}
